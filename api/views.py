ab7e32a2189bff841acab7671e127d2126b5697c
Joseph Adams <josadams27@gmail.com>
2023-03-01 23:33:51 +0000

    Cleaner attempt at avoiding reshuffling the project structure again

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
+import importlib
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from ..base.models import Item
-from ..base.serializers import ItemSerializer
+base = importlib.import_module("team40-22/base")
+from base.models import Item
+from base.serializers import ItemSerializer
 
 @api_view(['GET'])
 def getData(request):
