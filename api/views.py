7c668c8ac9fdcc33f0b595c326166d0d84245cb4
Joseph Adams <josadams27@gmail.com>
2023-03-01 23:26:29 +0000

    Slightly better attempt at avoiding reshuffling the project structure again

--- api/views.py
+++ api/views.py
@@ -1,9 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-import sys
-sys.path.insert(1, '/team40-22/base')
-from base.models import Item
-from base.serializers import ItemSerializer
+from ..base.models import Item
+from ..base.serializers import ItemSerializer
 
 @api_view(['GET'])
 def getData(request):
