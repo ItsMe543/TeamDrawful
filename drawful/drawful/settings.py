3c061285f4cdc110d5861976bdf6d7e8d86d0e5e
Joseph Adams <josadams27@gmail.com>
2023-03-04 20:56:19 +0000

    Added missing files and connected frontend rendering to backend server

--- drawful/drawful/settings.py
+++ drawful/drawful/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+REAL_BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'drawful.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(REAL_BASE_DIR, 'frontend', 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(REAL_BASE_DIR, 'frontend', 'build', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- drawful/drawfulApp/views.py
+++ drawful/drawfulApp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework import viewsets
 from .serializers import TodoSerializer
 from .models import Todo
+from django.views.decorators.csrf import requires_csrf_token
 
 # Create your views here.
 
-class TodoView(viewsets.ModelViewSet):
-    serializer_class = TodoSerializer
-    queryset = Todo.objects.all()
+#class TodoView(viewsets.ModelViewSet):#
+#    serializer_class = TodoSerializer
+#    queryset = Todo.objects.all()
+
+def main(request):
+    context = { }
+    return render(request, "index.html", context)
