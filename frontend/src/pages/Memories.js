6bcbc08dd96cbc00b323d68d334d33953b8edfac
Harry Parsons <hxp155@student.bham.ac.uk>
2023-03-06 19:47:15 +0000

    added calander library to memories page

--- frontend/src/pages/Memories.js
+++ frontend/src/pages/Memories.js
@@ -1,7 +1,22 @@
-import React from "react";
-
+import { React, useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { BsFillCalendarDateFill } from "react-icons/bs";
 function Memories() {
-  return <div>Memories Page!</div>;
+  const [date, setDate] = useState(new Date());
+  const onDateChange = (newDate) => {
+    setDate(newDate);
+  };
+  return (
+    <div className="memories">
+      <div className="m-titel">Memories Page!</div>
+      <BsFillCalendarDateFill size={30} />
+      <div className="calender">
+        <Calendar onChange={onDateChange} value={date} />
+      </div>
+      <div className="date">Date selected is: {date.toDateString()}</div>
+    </div>
+  );
 }
 
 export default Memories;
