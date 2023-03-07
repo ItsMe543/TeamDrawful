3d39d44437657dd6e192efdaed252ca7e00ada52
Harry Parsons <hxp155@student.bham.ac.uk>
2023-03-07 15:18:12 +0000

    Added icons and logo to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,28 +1,51 @@
 import React from "react";
+import { AiOutlineCalendar, AiOutlineHome } from "react-icons/ai";
+import { BsVectorPen } from "react-icons/bs";
+import { FaUserFriends } from "react-icons/fa";
+import { FiSettings } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import logo from "../drawful_light.png";
 import "../styles/Navbar.css";
 function Navbar() {
   // Navbar component
 
   return (
     <div className="navbar">
-      <div className="title">Drawful</div>
+      <div className="logo">
+        <img src={logo} alt="drawful logo" />
+      </div>
+      <div className="links">
+        <div className="home">
+          <Link to="/">
+            <AiOutlineHome size={25} />
+            <div>Home</div>
+          </Link>
+        </div>
 
-      <div className="home">
-        <Link to="/">Home</Link>
-      </div>
-
-      <div className="drawing">
-        <Link to="/drawing">Drawing</Link>
-      </div>
-      <div className="setttings">
-        <Link to="/settings">Settings</Link>
-      </div>
-      <div className="friends">
-        <Link to="/friends">Friends</Link>
-      </div>
-      <div className="memories">
-        <Link to="/memories">Memories</Link>
+        <div className="drawing">
+          <Link to="/drawing">
+            <BsVectorPen size={25} />
+            <div>Drawing</div>
+          </Link>
+        </div>
+        <div className="setttings">
+          <Link to="/settings">
+            <FiSettings size={25} />
+            <div>Settings</div>
+          </Link>
+        </div>
+        <div className="friends">
+          <Link to="/friends">
+            <FaUserFriends size={25} />
+            <div>Friends</div>
+          </Link>
+        </div>
+        <div className="memories">
+          <Link to="/memories">
+            <AiOutlineCalendar size={25} />
+            <div>Memories</div>
+          </Link>
+        </div>
       </div>
     </div>
   );
