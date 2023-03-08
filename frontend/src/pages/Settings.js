06198ce230b36c5e7a2157ae8dbfe1b8ba9d41bc
Abhay Uthappa <amu158@student.bham.ac.uk>
2023-03-08 20:47:04 +0000

    Fixed colour of text

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,27 +1,26 @@
 import React from "react";
-import Alert from '@mui/material/Alert';
 import Snackbar from '@mui/material/Snackbar';
 import Button from '@mui/material/Button';
 import LinearProgress from '@mui/material/LinearProgress';
 import { TextField } from "@mui/material";
 
 export default function Test() {
-
   const [state, setState] = React.useState({
-    open: false,
+    open: true,
     vertical: 'top',
     horizontal: 'center',
   });
 
   const { vertical, horizontal, open } = state;
 
-  const handleClick = (newState) => () => {
-    setState({ open: true, ...newState });
+  const handleClick = (newState, message) => () => {
+    setState({ ...state, open: true, ...newState, message });
   };
 
   const handleClose = () => {
     setState({ ...state, open: false });
   };
+
 
   const buttons = (
     <React.Fragment>
@@ -29,9 +28,30 @@ export default function Test() {
         onClick={handleClick({
           vertical: 'bottom',
           horizontal: 'left',
-        })}
+        }, "Changes saved")}
+        style={{
+          borderRadius: "5px", color: "white", backgroundColor: "rgba(0,120,255,255)",
+          border: "1px solid black", marginTop: "8px", textShadow: "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black"
+        }}
       >
         Save
+      </Button>
+      <Button
+        onClick={handleClick({
+          vertical: 'bottom',
+          horizontal: 'left',
+        }, "Changes cancelled")}
+        style={{
+          borderRadius: '5px',
+          color: 'white',
+          backgroundColor: 'rgba(246,71,71,1)',
+          border: '1px solid black',
+          marginTop: '8px',
+          marginLeft: '8px',
+          textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black',
+        }}
+      >
+        Cancel
       </Button>
     </React.Fragment>
   );
@@ -54,13 +74,21 @@ export default function Test() {
 
   return (
     <div style={{ textAlign: "center" }}>
-      <div style={{ textAlign: "left" }}>Settings Page!</div>
-      <br></br>
-      <Alert onClose={() => { }}>Its working!</Alert>
+      <style>
+        {`body{background-color: rgba(0,0,5, 0.85)}`}
+      </style>
+      <div style={{
+        textShadow: "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black",
+        textAlign: "left", color: "white"
+      }}>Settings</div>
       <br></br>
       <LinearProgress variant="determinate" value={progress} />
       <br></br>
-      <TextField id="outlined-basic" color="secondary"
+      <TextField id="outlined-basic" InputProps={{ style: { color: 'white' } }} InputLabelProps={{ style: { color: "white" } }}
+        style={{
+          borderRadius: "8px", backgroundColor:
+            "rgba(255, 255, 255, 0.08)", border: "1px solid black", textShadow: "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black"
+        }}
         label="Enter your username" variant="outlined" />
       <br></br>
       {buttons}
@@ -68,9 +96,18 @@ export default function Test() {
         anchorOrigin={{ vertical, horizontal }}
         open={open}
         onClose={handleClose}
-        message="Changes saved"
+        message={state.message}
         key={vertical + horizontal}
       />
+      <br></br>
+      <TextField id="outlined-basic" color="secondary"
+        label="Testing screen reader" InputProps={{ style: { color: 'white' } }}
+        style={{
+          borderRadius: "8px", marginTop: "100px", backgroundColor: "rgba(255, 255, 255, 0.08)",
+          border: "1px solid black", textShadow: "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black"
+        }}
+        InputLabelProps={{ style: { color: "white" } }} variant="outlined"
+        inputProps={{ "aria-label": "The ting goes skrrrahh, pap pap kah-kah-kah Skidiki-pap-pap, and a puu-puu-puurrr-boom Skiya, du-du-ku-ku-doom doom Poom poom, you dun now." }} />
     </div>
-  )
+  );
 }
