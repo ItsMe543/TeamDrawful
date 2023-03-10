d7929f7f7c7e09fd702fb004b543d631f109e7cc
Joseph Adams <josadams27@gmail.com>
2023-03-10 16:02:38 +0000

    Reintroduced database lines

--- drawful/drawful/settings.py
+++ drawful/drawful/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'drawful.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        "ENGINE": "django.db.backends.postgresql",
-#        "NAME": "drawful",
-#        "USER": "drawful",
-#        "PASSWORD": "drawful",
-#        "HOST": "localhost",
-#        "PORT": "5432",
-#    }
-#}
+DATABASES = {
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "drawful",
+        "USER": "drawful",
+        "PASSWORD": "drawful",
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
 
 
 # Password validation
