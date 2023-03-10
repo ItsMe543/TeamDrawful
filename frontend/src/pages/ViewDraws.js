e4d348f9bbb8140598aad75111ef54d34797d82c
Ty Bodin <txb111@student.bham.ac.uk>
2023-03-10 13:41:03 +0000

    Added in user profile details

--- frontend/src/pages/ViewDraws.js
+++ frontend/src/pages/ViewDraws.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import { AiOutlineStar } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import "../Fonts/Sometimes.otf"
@@ -7,52 +8,90 @@ import "../styles/ViewPage.css";
 
 // Ctrl + k to comment out region
 function ViewDrawings() {
-  const ColoredLine = ({ color }) => (
-    <hr
-        style={{
-            color: color,
-            backgroundColor: color,
-            height: 5
-        }}
-    />
-);
 const top = {
     // Other styles
     display: 'flex',
     flexDirection: 'row',
-}
+};
+
   return (
     <div>
       <h1 className="Title">
         Today's Pictures
       </h1>
 
+      <h2 className="PromptName">
+        Prompt: INSERT PROMPT
+      </h2>
+
       <div className="UserProfile">
           
         <div className="UserElement">
-          Drawing goes here
+          DRAWING GOES HERE
         </div>
 
         <div className="UserElement">
-          Loaing Userdeets...
-        </div>
+          <div className="UserName">
+            ExampleUser001
+          </div>
+          <div className="Stats">
+            TIME COMPLETED GOES HERE
+          </div>
+          <div className="Stats">
+            DIFFICULTY GOES HERE
+          </div>
+          <div className="Stats">
+            TIME TAKEN GOES HERE
+          </div>
 
-        <div className="UserElement">
-          Rating bar goes here
-        </div>
-        <div className="UserElement">
-        <div className="StarBar">
-          <div className="Star">
-            <AiOutlineStar size={20}/>
+          <div className="StarBar">
+            <Button className="Star1">
+              <AiOutlineStar size={20}/>
+            </Button>
+            <Button className="Star">
+              <AiOutlineStar size={20}/>
+            </Button>
+            <Button className="Star">
+              <AiOutlineStar size={20}/>
+            </Button>
+            <Button className="Star">
+              <AiOutlineStar size={20}/>
+            </Button>
+            <Button className="Star5">
+              <AiOutlineStar size={20}/>
+            </Button>
           </div>
         </div>
-        </div>
 
-
+        
         <div className="UserElement">
-          CommentSecion
+          Rate the drawing?
+          <div className="StarBar">
+            <Button className="Star1">
+              <AiOutlineStar size={35}/>
+            </Button>
+            <Button className="Star">
+              <AiOutlineStar size={35}/>
+            </Button>
+            <Button className="Star">
+              <AiOutlineStar size={35}/>
+            </Button>
+            <Button className="Star">
+              <AiOutlineStar size={35}/>
+            </Button>
+            <Button className="Star5">
+              <AiOutlineStar size={35}/>
+            </Button>
+          </div>
+        </div>
+        
+        <div className="UserElement">
+          <Button className="CommentButton">
+            CommentSecion
+          </Button>
         </div>
       </div>
+      
     </div>
   );
 }
