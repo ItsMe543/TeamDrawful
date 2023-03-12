6a89bd33a8d3b0ba3b244e45f892cf7e24fe3734
Abhay Uthappa <amu158@student.bham.ac.uk>
2023-03-12 18:06:45 +0000

    Settings Page Layout

---
+++ frontend/src/pages/Settings/Account.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Account() {
+    return (
+        <div className="account-container" style={{ paddingLeft: "250px" }}>
+            <div style={{ fontSize: '40px' }}>Account Settings</div>
+        </div>
+    )
+
+}
---
+++ frontend/src/pages/Settings/Security.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Security() {
+    return (
+        <div className="security-container" style={{ paddingLeft: "250px" }}>
+            <div style={{ fontSize: '40px' }}>Security Settings</div>
+        </div>
+    )
+
+}
