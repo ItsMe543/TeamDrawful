ffc708726f40d4b57330ffa51af835f6c0e50e6a
Tom Leigh <txl193@student.bham.ac.uk>
2023-03-21 14:36:56 +0000

    added axios stuff

--- drawful/drawfulApp/models.py
+++ drawful/drawfulApp/models.py
@@ -25,13 +25,13 @@ from django.db import models
 
 #Table for each users memories
 #Primary key = date
-class User_memories(models.Model):
+class User_Memories(models.Model):
         #memoryId = models.IntegerField(primary_key=True)
-        date = models.TimeField(primary_key=True)
+        date = models.DateField(primary_key=True)
         timeCompleted = models.TimeField()
         difficulty = models.CharField(max_length=10)
         avgRating = models.FloatField(default=0.0)
-        TimeTaken = models.TimeField()
+        timeTaken = models.IntegerField()
 
         #auto_now updates this field to the last time this object saves data
         prompt = models.CharField(max_length=50)
--- drawful/drawfulApp/serializers.py
+++ drawful/drawfulApp/serializers.py
@@ -11,3 +11,8 @@ class PromptSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Prompt_List
         fields = ('id', 'prompt', 'promptGenre', 'alreadyUsed', 'previousWinner')
+
+class User_MemoriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =models.User_Memories
+        fields =('date','timeCompleted','difficulty','avgRating','timeTaken','prompt','drawing')
