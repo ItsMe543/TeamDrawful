1969effa6fcd1575c9a2ca9e65b6c3fe3f7321b3
Harry Parsons <harryjohnparsons@outlook.com>
2023-03-21 00:16:14 +0000

    Created user memories GET and POST requests

--- drawful/drawfulApp/serializers.py
+++ drawful/drawfulApp/serializers.py
@@ -11,3 +11,9 @@ class PromptSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Prompt_List
         fields = ('id', 'prompt', 'promptGenre', 'alreadyUsed', 'previousWinner')
+
+
+class userMemoriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User_memories
+        fields = ('id', 'date', 'timeCompleted', 'difficulty', 'avgRating', 'TimeTaken', 'prompt', 'drawing')
