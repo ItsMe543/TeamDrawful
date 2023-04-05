797eaab47311fe932db60307f779782c47fb87bb
Joseph Adams <jxa111@student.bham.ac.uk>
2023-04-05 13:47:04 +0100

    Set up button's link to see friend's memories

--- frontend/src/pages/FriendsMemories.js
+++ frontend/src/pages/FriendsMemories.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import "../styles/Comments.css";
+import "../styles/FriendsMemories.css";
 
 function FriendsMemories() {
   //const {ID} = useParams();
   //const post = drawings[ID] 
-  //These are how i carried the user selected between pages in comments 
+  //These are how I carried the user selected between pages in comments 
   //I left them for reference if you wish to do them similarly
   return (
-    <div color="white">
+    <div className="TestDiv">
       Hello and welcome to the memories page!!
-     </div>
+    </div>
   )
 }
         
