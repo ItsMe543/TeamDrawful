cfee31207fb82a43271a6cbc4b48c3ee24e31257
Harry Parsons <hxp155@student.bham.ac.uk>
2023-04-16 22:36:51 +0000

    Revert "Shows when a user has a drawing for that day on calender"
    This reverts commit 70d5ea9ff5dcbf83edcf9d611c8a27b3265f9b39

--- frontend/src/drawingData.js
+++ frontend/src/drawingData.js
@@ -1,179 +1,187 @@
 const drawingData = [
-  {
-    id: 0,
-    username: "Harryparsons",
-    name: "Harry Parsons",
-    difficulty: 2,
-    email: "cooldude69@gmail.com",
-    bio: "Nothing here, IM MT",
-    completedDate: "10/03/2023",
-    completedTime: "17:58",
-    timeTaken: 58,
-    highestStreak: 4,
-    averageRating: 3.9,
-    totalStars: 50,
-    badgesUnlocked: 2,
-    mutualFriends: 3,
-    favouriteDraw: "/drawings/car.jpg",
-    badges: [
-      {
-        id: 0,
-        badgeIcon: "/drawings/star.png",
-      },
-      {
-        id: 1,
-        badgeIcon: "/drawings/award.png",
-      },
-    ],
-    prompt: "car",
-    avgRating: 3.8,
-    drawing: "/drawings/car.jpg",
-    Comments: [
-      {
-        id: 0,
-        UserCommenting: "JohnDoe",
-        Comment: "I like how you based this off the film cars!",
-      },
-      {
-        id: 1,
-        UserCommenting: "Xx69420mlgxX",
-        Comment: "So much detail wow!!",
-      },
-    ],
-  },
+    {
+        "id": 0,
+        "username": "Harryparsons",
+        "name": "Harry Parsons",
+        "difficulty": 2,
+        "email": "cooldude69@gmail.com",
+        "bio": "Nothing here, IM MT",
+        "completedDate": "10/03/2023",
+        "completedTime": "17:58",
+        "timeTaken": 58,
+        "highestStreak":4,
+        "averageRating":3.9,
+        "totalStars":50,
+        "badgesUnlocked":2,
+        "mutualFriends":3,
+        "favouriteDraw":"/drawings/car.jpg",
+        "badges":[
+            {
+                "id":0,
+                "badgeIcon":"/drawings/star.png",
+            },
+            {
+                "id":1,
+                "badgeIcon":"/drawings/award.png"
+            }
+        ],
+        "prompt": "car",
+        "avgRating": 3.8,
+        "drawing": "/drawings/car.jpg",
+        "Comments": [
+            {
+                "id": 0,
+                "UserCommenting": "JohnDoe",
+                "Comment": "I like how you based this off the film cars!",
+            },
+            {
+                "id": 1,
+                "UserCommenting": "Xx69420mlgxX",
+                "Comment": "So much detail wow!!",
+            }
+        ]
+    },
 
-  {
-    id: 1,
-    username: "JohnDoe",
-    difficulty: 3,
-    email: "SupaJD@outlook.co.uk",
-    bio: "This is my bio, my names John Doe, and i don't like bakers",
-    completedDate: "12/03/2023",
-    completedTime: "19:28",
-    timeTaken: 34,
-    highestStreak: 2,
-    averageRating: 4.5,
-    totalStars: 80,
-    badgesUnlocked: 1,
-    mutualFriends: 1,
-    favouriteDraw: "/drawings/house.jpg",
-    badges: [
-      {
-        id: 0,
-        badgeIcon: "/drawings/star.png",
-      },
-    ],
-    prompt: "house",
-    avgRating: 2.2,
-    drawing: "/drawings/house.jpg",
-    Comments: [
-      {
-        id: 0,
-        UserCommenting: "HarryParsons",
-        Comment: "Cool house but its too detailed",
-      },
-      {
-        id: 1,
-        UserCommenting: "Xx_drawingMastr69_xX",
-        Comment: "Would be better if you kept it simple",
-      },
-    ],
-  },
-  {
-    id: 2,
-    username: "Xx_drawingMastr69_xX",
-    difficulty: 4,
-    email: "DrwMstr6te9ne@gmail.com",
-    bio: "Oh yeah!! I am the drawing man myth and ledgend, watch me create absolute master pieces",
-    completedDate: "14/03/2023",
-    completedTime: "20:21",
-    timeTaken: 420,
-    highestStreak: 420,
-    averageRating: 2.4,
-    totalStars: 69,
-    badgesUnlocked: 2,
-    mutualFriends: 2,
-    favouriteDraw: "/drawings/minion.png",
-    badges: [
-      {
-        id: 0,
-        badgeIcon: "/drawings/star.png",
-      },
-      {
-        id: 1,
-        badgeIcon: "/drawings/award.png",
-      },
-    ],
-    prompt: "gru",
-    avgRating: 4.9,
-    drawing: "/drawings/minion.png",
-    Comments: [
-      {
-        id: 0,
-        UserCommenting: "HarryParsons",
-        Comment: "I love those films, great link!!",
-      },
-      {
-        id: 1,
-        UserCommenting: "JohnDoe",
-        Comment: "Such a cute minion",
-      },
-      {
-        id: 2,
-        UserCommenting: "itIsTy2323234",
-        Comment: "Could use some shading",
-      },
-    ],
-  },
-  {
-    id: 3,
-    username: "TyBee123",
-    name: "Ty Bodin",
-    difficulty: 4,
-    email: "example@eggmail.co.uk",
-    bio: "I am so eggselent with eggmail! This bio of course is just and eggsample, thats no eggsaduraton. My bio is truely eggsalant",
-    completedDate: "22/03/2023",
-    completedTime: "19:00",
-    timeTaken: 2,
-    highestStreak: 1,
-    averageRating: 4.9,
-    totalStars: 9,
-    badgesUnlocked: 0,
-    mutualFriends: 2,
-    favouriteDraw: "/drawings/car.jpg",
-    badges: [],
-    prompt: "car",
-    avgRating: 3.8,
-    drawing: "/drawings/car.jpg",
-    Comments: [{}],
-  },
-  {
-    id: 4,
-    username: "Jadams",
-    name: "Joe adams",
-    difficulty: 1,
-    email: "Jadaj@eggmail.com",
-    bio: "I have a longer bio to demsonstrate the lenght of the bio box better! Pfft",
-    completedDate: "01/01/2023",
-    completedTime: "18:12",
-    timeTaken: 43,
-    highestStreak: 123,
-    averageRating: 3.4,
-    totalStars: 690,
-    badgesUnlocked: 1,
-    mutualFriends: 2,
-    favouriteDraw: "/drawings/minion.png",
-    badges: [
-      {
-        id: 0,
-        badgeIcon: "/drawings/award.png",
-      },
-    ],
-    prompt: "car",
-    avgRating: 3.8,
-    drawing: "/drawings/minion.png",
-    Comments: [{}],
-  },
-];
+    {
+        "id": 1,
+        "username": "JohnDoe",
+        "difficulty": 3,
+        "email": "SupaJD@outlook.co.uk",
+        "bio": "This is my bio, my names John Doe, and i don't like bakers",
+        "completedDate": "10/03/2023",
+        "completedTime": "19:28",
+        "timeTaken": 34,
+        "highestStreak":2,
+        "averageRating":4.5,
+        "totalStars":80,
+        "badgesUnlocked":1,
+        "mutualFriends":1,
+        "favouriteDraw":"/drawings/house.jpg",
+        "badges":[
+            {
+                "id":0,
+                "badgeIcon":"/drawings/star.png",
+            }
+        ],
+        "prompt": "house",
+        "avgRating": 2.2,
+        "drawing": "/drawings/house.jpg",
+        "Comments": [
+            {
+                "id": 0,
+                "UserCommenting": "HarryParsons",
+                "Comment": "Cool house but its too detailed",
+            },
+            {
+                "id": 1,
+                "UserCommenting": "Xx_drawingMastr69_xX",
+                "Comment": "Would be better if you kept it simple",
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "username": "Xx_drawingMastr69_xX",
+        "difficulty": 4,
+        "email": "DrwMstr6te9ne@gmail.com",
+        "bio": "Oh yeah!! I am the drawing man myth and ledgend, watch me create absolute master pieces",
+        "completedDate": "11/03/2023",
+        "completedTime": "20:21",
+        "timeTaken": 420,
+        "highestStreak":420,
+        "averageRating":2.4,
+        "totalStars":69,
+        "badgesUnlocked":2,
+        "mutualFriends":2,
+        "favouriteDraw":"/drawings/minion.png",
+        "badges":[
+            {
+                "id":0,
+                "badgeIcon":"/drawings/star.png",
+            },
+            {
+                "id":1,
+                "badgeIcon":"/drawings/award.png"
+            }
+        ],
+        "prompt": "gru",
+        "avgRating": 4.9,
+        "drawing": "/drawings/minion.png",
+        "Comments": [
+            {
+                "id": 0,
+                "UserCommenting": "HarryParsons",
+                "Comment": "I love those films, great link!!",
+            },
+            {
+                "id": 1,
+                "UserCommenting": "JohnDoe",
+                "Comment": "Such a cute minion",
+            },
+            {
+                "id": 2,
+                "UserCommenting": "itIsTy2323234",
+                "Comment": "Could use some shading",
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "username": "TyBee123",
+        "name": "Ty Bodin",
+        "difficulty": 4,
+        "email": "example@eggmail.co.uk",
+        "bio": "I am so eggselent with eggmail! This bio of course is just and eggsample, thats no eggsaduraton. My bio is truely eggsalant",
+        "completedDate": "22/03/2023",
+        "completedTime": "19:00",
+        "timeTaken": 2,
+        "highestStreak":1,
+        "averageRating":4.9,
+        "totalStars":9,
+        "badgesUnlocked":0,
+        "mutualFriends":2,
+        "favouriteDraw":"/drawings/car.jpg",
+        "badges":[
+            
+        ],
+        "prompt": "car",
+        "avgRating": 3.8,
+        "drawing": "/drawings/car.jpg",
+        "Comments": [
+            {
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "username": "Jadams",
+        "name": "Joe adams",
+        "difficulty": 1,
+        "email": "Jadaj@eggmail.com",
+        "bio": "I have a longer bio to demsonstrate the lenght of the bio box better! Pfft",
+        "completedDate": "01/01/2023",
+        "completedTime": "18:12",
+        "timeTaken": 43,
+        "highestStreak":123,
+        "averageRating":3.4,
+        "totalStars":690,
+        "badgesUnlocked":1,
+        "mutualFriends":2,
+        "favouriteDraw":"/drawings/minion.png",
+        "badges":[
+            {
+                "id":0,
+                "badgeIcon":"/drawings/award.png"
+            }
+        ],
+        "prompt": "car",
+        "avgRating": 3.8,
+        "drawing": "/drawings/minion.png",
+        "Comments": [
+            {
+            }
+        ]
+    }
+]
 
 export default drawingData;
