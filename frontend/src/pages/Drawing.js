bfacfc6c5bf9c4c13e51df8c5d7eee8f8f36d66d
Tom Leigh <txl193@student.bham.ac.uk>
2023-04-20 12:12:02 +0100

    changed homepage design and added timer to drawing page

--- frontend/src/pages/Drawing.js
+++ frontend/src/pages/Drawing.js
@@ -170,6 +170,35 @@ class canFill {
 }
 
 
+
+window.onload = function() {
+  document.getElementById("Label1").innerHTML="1:00";
+
+    Label1 = window.setInterval(function() {
+      myFunction();
+    }, 1000); // every second
+
+}
+
+
+///////////////////////////////////////////////////////////
+var seconds=60;
+var Label1;
+function myFunction() {
+  if(seconds < 60) { // I want it to say 1:00, not 60
+    document.getElementById("Label1").innerHTML = seconds;
+  }
+  if (seconds >0 ) { // so it doesn't go to -1
+     seconds--;
+     console.log(seconds)
+  } else {
+     clearInterval(Label1);
+     alert("You type X WPM");
+  }
+}
+///////////////////////////////////////////////////////////
+
+
 function Drawing() {
     const [date,setName,difficulty] = useState({
     date: " ",difficulty: " "
@@ -437,7 +466,7 @@ function Drawing() {
         ></canvas>
 
         <div className="time">
-          *Time here
+          <label ID="Label1" runat="server"></label>
         </div>
 
 
@@ -449,5 +478,7 @@ function Drawing() {
     </div >
   );
 }
-
 export default Drawing;
+
+//Getdate()
+
