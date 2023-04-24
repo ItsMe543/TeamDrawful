8dec9c8dad496abc0271c65ec47f184f8ca226da
Abhay Uthappa <amu158@student.bham.ac.uk>
2023-04-24 23:19:04 +0100

    Testing editing

--- frontend/src/pages/Settings/Account.js
+++ frontend/src/pages/Settings/Account.js
@@ -17,37 +17,53 @@ export default function Account() {
     });
 
     useEffect(() => {
-        const username = "DrawingGod69";
-        axios.get(`/api/user_accounts/${username}/`).then((data) => {
+        const email = "test";
+        //axios.get(`/api/user_accounts/${email}/`).then((data) => {
+        axios.get(`/api/user_accounts/1/`).then((data) => {
             console.log(data);
             setPost(data?.data);
         });
     }, []);
 
     function deleteAccount() {
-        const username = "DrawingGod69";
-        axios.delete(`/api/user_accounts/${username}/`)
-            .then(response => {
+        const email = "test";
+        axios
+            //.delete(`/api/user_accounts/${email}/`)
+            .delete(`/api/user_accounts/1/`)
+            .then((response) => {
                 console.log(response);
-
             })
-            .catch(error => {
+            .catch((error) => {
                 console.log(error);
-
             });
     }
+
+    function updateAccount() {
+        const email = "test";
+        axios
+            //.put(`/api/user_accounts/${email}/`, post)
+            .put(`/api/user_accounts/1/`, post)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+
 
     return (
         <div className="account-container">
             <Col>
                 <Row>
-                    <Col md={2.5} style={{ paddingLeft: '30px' }} >
+                    <Col md={2.5} style={{ paddingLeft: "30px" }}>
                         <Link to="/memories">
-                            <img src="/drawings/car.jpg" />
+                            <img src="/drawings/car.jpg" alt="car" />
                         </Link>
                     </Col>
-                    <Col style={{ paddingLeft: '30px' }}>
-                        <div className="account-header" >Account Settings</div>
+                    <Col style={{ paddingLeft: "30px" }}>
+                        <div className="account-header">Account Settings</div>
                         <div className="sub">Edit your personal Settings</div>
                     </Col>
                 </Row>
@@ -58,25 +74,24 @@ export default function Account() {
                         <div className="labels">Email Address</div>
                         <div className="labels">Bio</div>
                     </Col>
-                    <Col >
+                    <Col>
                         <div className="input">
                             <input
-
                                 placeholder="Enter your name"
                                 value={post.name}
+                                onChange={(e) => setPost({ ...post, name: e.target.value })}
                             />
                         </div>
                         <div className="input">
                             <input
-
                                 placeholder="Enter your username"
                                 value={post.username}
+                                onChange={(e) => setPost({ ...post, username: e.target.value })}
                             />
                         </div>
-                        <div className="input" >
+                        <div className="input">
                             <input
-                                style={{ color: 'rgb(146,146,146)' }}
-
+                                style={{ color: "rgb(146,146,146)" }}
                                 placeholder="email id"
                                 value={post.email}
                                 readOnly="true"
@@ -84,11 +99,11 @@ export default function Account() {
                         </div>
                         <div className="input">
                             <textarea
-
                                 placeholder="Max No. of Characters: 256s"
                                 value={post.bio}
                                 style={{ resize: "none" }}
                                 maxLength={256}
+                                onChange={(e) => setPost({ ...post, bio: e.target.value })}
                             />
                         </div>
                     </Col>
@@ -99,9 +114,6 @@ export default function Account() {
                 <div className="badges-container"></div>
 
                 <Row>
-                    {/* <Col md="3" style={{ border: '1px solid white' }}>
-                        <button>Save Changes</button>
-    </Col>*/}
                     <Col>
                         <div className="stats-header">Stats</div>
                         <div className="stats">
@@ -109,7 +121,9 @@ export default function Account() {
                             <div>Average Rating: {post.averageRating}</div>
                             <div>Total Stars Earned: {post.totalStars} </div>
                             <div>Badges Unlocked: {post.badgesEarned}</div>
-                            <button onClick={deleteAccount}>Temporary Delete</button>
+                            {/*<button onClick={deleteAccount}>Delete Account</button>*/}
+                            <button onClick={updateAccount}>Save Changes</button>
+
                         </div>
                     </Col>
 
