153d7f40cc96ddc3d2085f891984730d37061d06
Alex Sadler <axs1949@student.bham.ac.uk>
2023-04-24 18:50:06 +0100

    added search bar filtering badges to searchterm

--- frontend/src/pages/Badges.js
+++ frontend/src/pages/Badges.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/Badges.css"
 import { Col, Row } from "react-bootstrap";
+import { useState } from "react";
 
 
 function Badges() {
-  
+
   const badgesArray = [
     // example badges
     {
@@ -58,34 +59,23 @@ function Badges() {
     }
   ];
 
-  const [activeSortButton, setActiveSortButton] = React.useState(null);
-  const [sortedBadgesArray, setSortedBadgesArray] = React.useState(badgesArray);
-
-  React.useEffect(() => {
-    setSortedBadgesArray(badgesArray);
-  }, []);
-
-  const handleSort = (option) => {
-    setActiveSortButton(option);
-    // sort the data based on the selected option
-    if (option === "rarest") {
-      // sort by rarest
-    } else if (option === "recent") {
-      // sort by most recent
-      console.log("recent");
-      const sortedBadges = [...sortedBadgesArray].sort((a, b) => a.timeUnlocked - b.timeUnlocked);
-      setSortedBadgesArray(sortedBadges);
-    } else {
-      // sort by oldest
-      console.log("oldest");
-      const sortedBadges = [...sortedBadgesArray].sort((a, b) => b.timeUnlocked - a.timeUnlocked);
-      setSortedBadgesArray(sortedBadges);
-      
-    }
-  };
-
-
-  
+  function createBadgeElements(badges) {
+    return (
+        <Col>
+          {badges.map((badge, id) => (
+            <div className="badgeElement" key={id}>
+              <Badge 
+                name={badge.name} 
+                image={badge.image} 
+                unlocked={badge.unlocked} 
+                description={badge.description} 
+                timeUnlocked={badge.timeUnlocked} 
+              />
+            </div>
+          ))}
+        </Col>
+    );
+  }
 
   function Badge(props) {
     const { name, image, unlocked, description} = props;
@@ -107,59 +97,92 @@ function Badges() {
       </div>
     );
   }
+
+  const [activeSortButton, setActiveSortButton] = React.useState(null);
+  const [sortedBadgesArray, setSortedBadgesArray] = React.useState(badgesArray);
+
+  
   
 
-  function createBadgeElements(badges) {
-    return (
-        <Col>
-          {badges.map((badge, id) => (
-            <div className="badgeElement" key={id}>
-              <Badge name={badge.name} image={badge.image} unlocked={badge.unlocked} description={badge.description} timeUnlocked={badge.timeUnlocked} />
-            </div>
-          ))}
-        </Col>
+  const handleSort = (option) => {
+    setActiveSortButton(option);
+    // sort the data based on the selected option
+    if (option === "rarest") {
+      // sort by rarest
+    } else if (option === "recent") {
+      // sort by most recent
+      console.log("recent");
+      const sortedBadges = [...sortedBadgesArray].sort((a, b) => a.timeUnlocked - b.timeUnlocked);
+      setSortedBadgesArray(sortedBadges);
+    } else {
+      // sort by oldest
+      console.log("oldest");
+      const sortedBadges = [...sortedBadgesArray].sort((a, b) => b.timeUnlocked - a.timeUnlocked);
+      setSortedBadgesArray(sortedBadges);
+      
+    }
+  };
+  
+
+  //search bar
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredBadges, setFilteredBadges] = useState(sortedBadgesArray);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+    const filteredBadges = sortedBadgesArray.filter((filteredBadge) =>
+      filteredBadge.name.toLowerCase().includes(event.target.value.toLowerCase()) ||
+      filteredBadge.description.toLowerCase().includes(event.target.value.toLowerCase())
     );
-  }
-  
+    console.log(event.target.value);
+    console.log(filteredBadges);
+    setFilteredBadges(filteredBadges);
+  };
 
-  const unlockedBadgesIcons = sortedBadgesArray.filter(badge => badge.unlocked);
-  const lockedBadgesIcons = sortedBadgesArray.filter(badge => !badge.unlocked);
+  // seperates badges into unlocked and locked with filter and sort
+  const unlockedBadges = sortedBadgesArray.filter(badge => badge.unlocked && filteredBadges.includes(badge));
+  const lockedBadges = sortedBadgesArray.filter(badge => !badge.unlocked && filteredBadges.includes(badge));
 
   return (
     <div className="Badges">
 
       <div className="filtersBar">
-          <h2 className="filters" id="button">
-            Filters
-          </h2>
-          <svg className="seperator">
-            <rect></rect>
-          </svg> 
+        <h2 className="filters" id="button">
+          Filters
+        </h2>
+        <svg className="seperator">
+          <rect></rect>
+        </svg> 
 
-          <div className="sortButtons">
-            <button
-              className={`sortRarest ${activeSortButton === "rarest" ? "toggled" : ""}`}
-              onClick={() => {
-                handleSort("rarest")}}
-            >
-              Rarest
-            </button>
-            <button
-              className={`sortRecent ${activeSortButton === "recent" ? "toggled" : ""}`}
-              onClick={() => {
-                handleSort("recent")}}
-            >
-              Most Recent
-            </button>
-            <button
-              className={`sortOldest ${activeSortButton === "oldest" ? "toggled" : ""}`}
-              onClick={() => {
-                handleSort("oldest")}}
-            >
-              Oldest
-            </button>
-          </div>
+        <div className="searchBar">
+          <input type="text" value={searchTerm} onChange={handleSearch} placeholder="Filter Badges..."/>
         </div>
+
+        
+        <div className="sortButtons">
+          <button
+            className={`sortRarest ${activeSortButton === "rarest" ? "toggled" : ""}`}
+            onClick={() => {
+              handleSort("rarest")}}
+          >
+            Rarest
+          </button>
+          <button
+            className={`sortRecent ${activeSortButton === "recent" ? "toggled" : ""}`}
+            onClick={() => {
+              handleSort("recent")}}
+          >
+            Most Recent
+          </button>
+          <button
+            className={`sortOldest ${activeSortButton === "oldest" ? "toggled" : ""}`}
+            onClick={() => {
+              handleSort("oldest")}}
+          >
+            Oldest
+          </button>
+        </div>
+      </div>
 
       <div className="badgesContainer">
         <div className="badgesUnlocked">
@@ -171,7 +194,7 @@ function Badges() {
           </svg>
           <div className="badgeElement">
             <Row>
-              {createBadgeElements(unlockedBadgesIcons)}
+              {createBadgeElements(unlockedBadges)}
             </Row>
           </div>
         </div>
@@ -185,7 +208,7 @@ function Badges() {
           </svg>
           <div className="badgeElement">
             <Row>
-              {createBadgeElements(lockedBadgesIcons)}
+              {createBadgeElements(lockedBadges)}
             </Row>
           </div>
         </div>
