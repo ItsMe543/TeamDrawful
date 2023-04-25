9e1eb9c6d3d97a125731870ab7727ae75d71129d
Ty Bodin <txb111@student.bham.ac.uk>
2023-04-25 15:33:09 +0100

    Added detail checks

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import "../styles/SignUpAndLogIn.css";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import PasswordInput from "./PasswordInput";
+import { useState } from "react";
 
 //<div className="info-label">Password</div>
 //<input className="info-box" type="text" placeholder="Password" />
@@ -10,38 +10,50 @@ import PasswordInput from "./PasswordInput";
 
 function Login() {
   //<div style={{ fontSize: "50px", color: "white" }}>
+  const[password, setPassword] = useState("");
+  const[visible, setVisible] = useState(false);
   return (
-    <div className="Background">
-      <div className="accounts-page">
-        <div className="account-details">
-          <div className="title-text">
-            Please Log in
+    
+      <div className="page">
+          <div className="accounts-page">
+            <div className="account-details">
+              <div className="title-text">
+                Log in
+              </div>
+
+              <div className="details-segment">
+                  <div className="info-label">Username</div>
+                  <input className="info-box" type="text" placeholder="Username" />
+              </div>
+
+              <div className="details-segment">
+                <form>
+                  <label htmlFor="password" className="infoP-label">
+                    Password
+                  </label>
+                  <div className="infoP-box">
+                    <input value={password} type={visible ? "text" : "password"} id="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} className="infoP-box">
+                    </input>
+                  </div>
+                </form>
+              </div>
+
+              <Link className="sign-up-link" to={"/signUp/"}>
+                Create an account?
+              </Link>
+
+              <button className="submit-acc-details">
+                <Link to={"/home/"}>
+                  Log in
+                </Link>
+              </button>
+            </div>
           </div>
-
-          <div className="details-segment">
-              <div className="info-label">Username</div>
-              <input className="info-box" type="text" placeholder="Username" />
+          <div className="disclaimer1">
+              "Alpha Project Disclaimer This server is provided by the School of Computer Science at the University of Birmingham to allow users to provide feedback on software developed by students as part of an assignment. While we take reasonable precautions, we cannot guarantee the security of the data entered into the system. Do NOT enter any real personal data (e.g., financial information or otherwise) into the system. The assignment runs until May 31st 2023, at which time the server and all associated data will be destroyed."
           </div>
-
-          <div className="details-segment">
-              <PasswordInput/>
-          </div>
-
-          <Link className="sign-up-link" to={"/signUp/"}>
-            Create an account?
-          </Link>
-
-          <button className="submit-acc-details">
-            <Link to={"/home/"}>
-              Log in
-            </Link>
-          </button>
-        </div>
-        <div className="disclaimer1">
-          "Alpha Project Disclaimer This server is provided by the School of Computer Science at the University of Birmingham to allow users to provide feedback on software developed by students as part of an assignment. While we take reasonable precautions, we cannot guarantee the security of the data entered into the system. Do NOT enter any real personal data (e.g., financial information or otherwise) into the system. The assignment runs until May 31st 2023, at which time the server and all associated data will be destroyed."
-        </div>
       </div>
-    </div>
+    
   );
 }
 export default Login;
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -2,13 +2,17 @@ import React from "react";
 import "../styles/SignUpAndLogIn.css";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import PasswordInput from "./PasswordInput";
-import ConfirmPassword from "./ConfirmPassword"
 import axios from "axios"; //Used for database interactivity
 import { useEffect, useState } from "react";
 
 function SignUp() {
   //<div style={{ fontSize: "50px", color: "white" }}>
+  const[password, setPassword] = useState("");
+  const[visible, setVisible] = useState(false);
+
+  const[confirmPassword, setConfirmPassword] = useState("");
+  const[cpVisible, setCPVisible] = useState(false);
+
   const [accountDetails, setAccountDetails] = useState({
     username: "",
     password: "",
@@ -54,7 +58,7 @@ function SignUp() {
       method: 'post',
       url: '/api/user_accounts/',
       data: {
-        username: fullname,
+        username: accountUserName,
         password: accountHashedPass,
         first_name: fullname.split(" ")[0],
         last_name: fullname.split(" ")[1],
@@ -81,21 +85,20 @@ function SignUp() {
   const checkUserDetails = (e) => {
     e.preventDefault();
     //Check fullname (at least 2 letters/spaces, no symbols or numbers)
-    if ((fullname.length) < 2 || fullname.match(/[0-9]/ || /[',./?@;:{}=+-_)(*&^%$£"!¬`¦\|><[]]/)){ //
-      console.log("YOUR NAME IS BAD!");
+    if ((fullname.length) < 2 || fullname.match(/[0-9]/ || /[',./?@;:{}=+-_)(*&^%$£"!¬`¦\|><[]]/)){
       return;
     }
     //console.log("You have a wonderful name :)");
 
     //check username (at least 8 characters, no duplicate usernames)
-    if ((accountUserName.length < 8)){
-      console.log("Username NOT allowed");
+    if ((accountUserName.length < 8 || accountUserName.match(/[/\s/g]/))){
       return;
     }
 
+    if ()
+
     //check email (Real email adress, no duplicate emails)
     if ((accountEmail.length < 1) || !(accountEmail.match(/[@]/))){
-      console.log("No way thats a real email");
       return;
     }
 
@@ -104,6 +107,8 @@ function SignUp() {
     //Check password (at least 8 characters, at least 1 number)
 
     //If all checks are passed, send details to database 
+
+    setAccountHashedPass(password);
     sendNewDetails(); 
   }
 
@@ -111,49 +116,64 @@ function SignUp() {
   
   return (
     <div className="page">
-    <div className="accounts-page">
-      <div className="account-details">
-        <div className="title-text">
-          Create an account
+      <div className="accounts-page">
+        <div className="account-details">
+          <div className="title-text">
+            Create an account
+          </div>
+
+          <div className="details-segment">
+              <div className="info-label">Full Name</div>
+              <input className="info-fullname-box" type="text" placeholder="Fullname" onChange={e => setFullName(e.target.value)}/>
+          </div>
+
+          <div className="details-segment">
+              <div className="info-label">Username</div>
+              <input className="info-username-box" type="text" placeholder="Username" onChange={e => setAccountUsername(e.target.value)}/>
+          </div>
+
+          <div className="details-segment">
+              <div className="info-label">Email</div>
+              <input className="info-email-box" type="text" placeholder="ExampleUser@examplemail.co.uk" onChange={e => setAccountEmail(e.target.value)}/>
+          </div>
+
+          <div className="details-segment">
+            <form>
+              <label htmlFor="password" className="infoP-label">
+                Password
+              </label>
+              <div className="infoP-box">
+                <input value={password} type={visible ? "text" : "password"} id="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} className="infoP-box">
+                </input>
+              </div>
+            </form>
+          </div>
+
+          <div className="details-segment">
+            <form>
+              <label htmlFor="password" className="infoP-label">
+              Confirm Password
+              </label>
+              <div className="infoP-box">
+                <input value={confirmPassword} type={cpVisible ? "text" : "password"} id="password" placeholder="Password" onChange={(e) => setConfirmPassword(e.target.value)} className="infoP-box">
+                </input>
+              </div>
+            </form>
+          </div>
+
+          <Link className="login-signup-switch" to={"/login/"}>
+              Already got an account? Login
+          </Link>
+
+          <button className="submit-acc-details" onClick={checkUserDetails}>
+            Create account
+          </button>
         </div>
-
-        <div className="details-segment">
-            <div className="info-label">Full Name</div>
-            <input className="info-fullname-box" type="text" placeholder="Fullname" onChange={e => setFullName(e.target.value)}/>
-        </div>
-
-        <div className="details-segment">
-            <div className="info-label">Username</div>
-            <input className="info-username-box" type="text" placeholder="Username" onChange={e => setAccountUsername(e.target.value)}/>
-        </div>
-
-        <div className="details-segment">
-            <div className="info-label">Email</div>
-            <input className="info-email-box" type="text" placeholder="ExampleUser@examplemail.co.uk" onChange={e => setAccountEmail(e.target.value)}/>
-        </div>
-
-        <div className="details-segment">
-            <div className="info-label">Password</div>
-            <input className="info-pass-box" type="text" placeholder="Password" onChange={e => setAccountHashedPass(e.target.value)}/>
-        </div>
-
-        <div className="details-segment">
-          <ConfirmPassword/>
-        </div>
-
-        <Link className="login-signup-switch" to={"/login/"}>
-            Already got an account? Login
-        </Link>
-
-        <button className="submit-acc-details" onClick={checkUserDetails}>
-          Create account
-        </button>
+      </div>
+      <div className="disclaimer1">
+        "Alpha Project Disclaimer This server is provided by the School of Computer Science at the University of Birmingham to allow users to provide feedback on software developed by students as part of an assignment. While we take reasonable precautions, we cannot guarantee the security of the data entered into the system. Do NOT enter any real personal data (e.g., financial information or otherwise) into the system. The assignment runs until May 31st 2023, at which time the server and all associated data will be destroyed."
       </div>
     </div>
-    <div className="disclaimer1">
-      "Alpha Project Disclaimer This server is provided by the School of Computer Science at the University of Birmingham to allow users to provide feedback on software developed by students as part of an assignment. While we take reasonable precautions, we cannot guarantee the security of the data entered into the system. Do NOT enter any real personal data (e.g., financial information or otherwise) into the system. The assignment runs until May 31st 2023, at which time the server and all associated data will be destroyed."
-    </div>
-  </div>
   );
 }
 export default SignUp;
