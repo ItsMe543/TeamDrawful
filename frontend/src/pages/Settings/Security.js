763a705403cc0da93e3a105e34ba941d0a552fd4
Abhay Uthappa <amu158@student.bham.ac.uk>
2023-04-25 16:58:08 +0100

    High contrast mode changes

--- frontend/src/pages/Settings/Security.js
+++ frontend/src/pages/Settings/Security.js
@@ -1,7 +1,7 @@
 import React from "react";
 export default function Security() {
     return (
-        <div className="security-container" style={{ paddingLeft: "250px" }}>
+        <div className="security-container">
             <div style={{ fontSize: '40px' }}>Security Settings</div>
         </div>
     )
