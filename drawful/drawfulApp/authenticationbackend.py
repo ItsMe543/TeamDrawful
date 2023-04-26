ed2962504d2ccd26019eaff2ccb20078bd171045
Joseph Adams <jxa111@student.bham.ac.uk>
2023-04-26 22:53:30 +0100

    FORGOT TO ADD A FILE

---
+++ drawful/drawfulApp/authenticationbackend.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.hashers import check_password
+from drawfulApp.models import User_Accounts
+
+class CustomBackend(BaseBackend):
+    def authenticate(self, username, password):
+        try:
+            user = User_Accounts.objects.get(username=username)
+
+            if check_password(password, user.password):
+                return user
+            else:
+                return None
+        except User_Accounts.DoesNotExist:
+            return None
+    
+    def get_user(self, user_id):
+        try:
+            return User_Accounts.objects.get(pk=user_id)
+        except:
+            return None
