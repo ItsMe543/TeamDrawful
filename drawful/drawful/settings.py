676af1ba9dd6dde5b43308f2ffbe4ea799b8277a
Thomas Leigh <txl193@student.bham.ac.uk>
2023-04-29 23:59:55 +0000

    Update settings.py

--- drawful/drawful/settings.py
+++ drawful/drawful/settings.py
@@ -123,7 +123,7 @@ PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
     "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
-    "django.contrib.auth.hashers.ScryptPasswordHasher",
+    #"django.contrib.auth.hashers.ScryptPasswordHasher",
 ]
 
 
