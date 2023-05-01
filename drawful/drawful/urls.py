df0337c688483334cf65e641e694410d0d725fba
Harry Parsons <hxp155@student.bham.ac.uk>
2023-05-01 11:18:33 +0000

    bruh

--- drawful/drawful/urls.py
+++ drawful/drawful/urls.py
@@ -1,4 +1,4 @@
-    """drawful URL Configuration
+"""drawful URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
